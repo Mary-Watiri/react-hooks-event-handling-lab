f2bca81a98daaa081dd5ca33529aff54fe7152b2
Mary Watiri <watirimary30@gmail.com>
2024-01-31 18:33:05 +0300

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
+import React from "react";
+
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function handleBlur(event){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return(
+        <button onFocus={() => console.log("Good!")} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
